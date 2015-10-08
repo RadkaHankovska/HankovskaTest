c3ee95b8bf33a8db97b9eb91fcc0db2d200a5901
RadkaHankovska <radka.hankovska@gmail.com>
2015-10-08 19:51:48 +0200

    Projekt s databázou - ListOfEmployeesMVC
    ListOfEmployeesMVC som vytvorila v MS Visual Studio 2012. Databázu v MS
    SQL 2014. Tú som následne dettach-la a priložila k projektu, treba ju
    attach-nut k serveru.
    Je použitá architektúra MVC4 a dáta sú ukladané cez Entity Framework.
    Webové rozhranie (ASP.NET MVC) priamo zobrazuje zoznam zamestnancov,
    ktorý vieme už aj editovať, pozerať si detaily a vymazávať zamestnancov.
    Funkcia delete je ošetrená a nemaže sa priamo cez post request (pre
    prípad chybného kliknutia). Máme aj funkciu na pridávanie nových
    záznamov.
    Vďaka za všetky pripomienky a feedback :).

---
+++ ListOfEmployeesMVC/MvcApplication1/Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+using MvcApplication1.Models;
+
